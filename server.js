cb75b41aec49da903bfc8e547618c2cb07150390
Boshra Jaber <boshrajaber94@gmail.com>
2022-02-08 11:50:22 +0200

    Async-Await vs then

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 const express = require('express');
-
+const axios = require("axios");
+const { get } = require('http');
 
 
 const app = express();
@@ -14,6 +15,8 @@ app.get('/', handleHome);
 app.get('/users', handleQuery);
 app.get("/user/:id", handleParams);
 app.post("/userInfo", handleBody);
+app.get("/anime", handleAnime); // using .then().catch()
+app.get("/anime/v2", handleAnimeV2) // using async - await
 
 
 // functions
@@ -31,17 +34,57 @@ function handleQuery(req, res) {
 }
 // http://localhost:3000/user/1
 // req.params : params is part of the req, it is a keyword
-function handleParams(req,res){
+function handleParams(req, res) {
     console.log(req.params)
     res.send("I am working");
 }
 //http://localhost:3000/userInfo
 // req.body : body is part of the req, it is a keyword
-function handleBody(req,res){
+function handleBody(req, res) {
     console.log(req.body);
     res.json(req.body);
 }
+//3rd API: https://api.jikan.moe/v4/anime/1
+// http://localhost:3000/anime
+// axios.get().then().catch();
+function handleAnime(req, res) {
+    console.log(1, "before then");
+    axios.get("https://api.jikan.moe/v4/anime/1").then(result => {
+        // console.log(result.data.data);
+        console.log(2, "Inside then");
+        let animeObject = result.data.data;
+        let anime = new Anime(animeObject)
+        // res.send("Working");
+        res.json(anime);
+    }).catch(error => {
+        console.log(error);
+    })
+    console.log(3, "after then");
+}
+// Async - await
+// http://localhost:3000/anime/v2
+// try {} catch (error){}
+async function handleAnimeV2(req, res) {
+    try {
+        console.log(1, "before await");
+        let result = await axios.get("https://api.jikan.moe/v4/anime/1");
+        console.log(2, "after await");
+        // console.log(result.data.data);
+        let animeObject = result.data.data;
+        let anime = new Anime(animeObject)
+        res.json(anime)
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
 
+
+// constructor
+function Anime(animeInfo) {
+    this.title = animeInfo.title;
+    this.episodes = animeInfo.episodes;
+}
 
 app.listen(PORT, () => {
     console.log(`Example app listening on port ${PORT}`)
